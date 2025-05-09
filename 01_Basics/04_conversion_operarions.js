234e4b2e302268ad8df5a062177885a4d6202c40
jhaayush2004 <shauryasphinx@gmail.com>
2025-05-09 20:26:48 +0530

    Add conversion operations file

---
+++ 01_Basics/04_conversion_operarions.js
@@ -0,0 +1,39 @@
+let value  =3
+let negvalue = -value
+console.log(negvalue) // -3
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = "Ayush"
+let str2 = "Jha"
+let str3 = str1 + " " + str2
+console.log(str3) // Ayush Jha
+
+console.log("1" + 2) // 12
+console.log("1" + "2") // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+// First data type is given priority for conversion operations
+
+console.log(+true) //1
+console.log(+"") //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+//Read: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
