83884d47cf396afb03471e4a837d60ce45fe13e0
jhaayush2004 <shauryasphinx@gmail.com>
2025-05-25 12:48:38 +0530

    changes

--- 02_Basics/objects2.js
+++ 02_Basics/objects2.js
@@ -1,62 +1,62 @@
-// const objOne = new Object() //singleton onject
-// const objTwo = {} // non singleton object
-// //this is the only difference b/w two , banki kahani same hai
+const objOne = new Object() //singleton onject
+const objTwo = {} // non singleton object
+//this is the only difference b/w two , banki kahani same hai
 
-// const tinderUser = {}
-// tinderUser.id = "123abc"
-// tinderUser.name = "Sama"
-// tinderUser.isLoggedIn = false
-// console.log(tinderUser) // { id: '123abc', name: 'Sama', isLoggedIn: false }
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "Sama"
+tinderUser.isLoggedIn = false
+console.log(tinderUser) // { id: '123abc', name: 'Sama', isLoggedIn: false }
 
-// const regularUser = {
-//     email : "some@gmail.com",
-//     fullname : {
-//         userfullname: {
-//             firstname: "Ayush",
-//             middlename: "Shaurya",
-//             Lastname: "Jha"
-//         }
-//     }
-// }
-// console.log(regularUser) 
-// /* {
-//   email: 'some@gmail.com',
-//   fullname: {
-//     userfullname: { firstname: 'Ayush', middlename: 'Shaurya', Lastname: 'Jha' }
-//   }
-// }
-// */
-// console.log(regularUser.fullname.userfullname.Lastname); // Jha
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname: {
+            firstname: "Ayush",
+            middlename: "Shaurya",
+            Lastname: "Jha"
+        }
+    }
+}
+console.log(regularUser) 
+/* {
+  email: 'some@gmail.com',
+  fullname: {
+    userfullname: { firstname: 'Ayush', middlename: 'Shaurya', Lastname: 'Jha' }
+  }
+}
+*/
+console.log(regularUser.fullname.userfullname.Lastname); // Jha
 
-// const obj1 = {1: "a", 2: "b"}
-// const obj2 = {3: "a", 4: "b"}
-// const obj3 = {5: "a", 6: "b"}
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
 
-// const obj4 = {obj1, obj2};
-// console.log(obj4) // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }  
+const obj4 = {obj1, obj2};
+console.log(obj4) // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }  
 
-// const obj5 = Object.assign(obj1, obj2)
-// console.log(obj5); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
-// console.log(obj1); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' } // problem with this is that it changes obj1 also bcoz  Object.assign(target, source); so target is genrally kept as empty object
-// // Visit object.assign mdn docs
+const obj5 = Object.assign(obj1, obj2)
+console.log(obj5); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+console.log(obj1); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' } // problem with this is that it changes obj1 also bcoz  Object.assign(target, source); so target is genrally kept as empty object
+// Visit object.assign mdn docs
 
-// //if you dont want to modify any excisting object use {} with assign
-// const obj6 = Object.assign({}, obj2, obj3) //here we get all objects into {} object
-// console.log(obj5); // { '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+//if you dont want to modify any excisting object use {} with assign
+const obj6 = Object.assign({}, obj2, obj3) //here we get all objects into {} object
+console.log(obj5); // { '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
-// //**better way: use spread operator**
-// const newObj = {...obj2, ...obj3}
-// console.log(newObj) // { '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+//**better way: use spread operator**
+const newObj = {...obj2, ...obj3}
+console.log(newObj) // { '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
-// console.log(Object.keys(tinderUser));
-// //[ 'id', 'name', 'isLoggedIn' ] --returns all keys as an array
-// console.log(Object.values(tinderUser));
-// //[ '123abc', 'Sama', false ]--returns all values of object as an array
-// console.log(Object.entries(tinderUser));
-// //[ [ 'id', '123abc' ], [ 'name', 'Sama' ], [ 'isLoggedIn', false ] ]--returns [key,value] pair as an array of array
+console.log(Object.keys(tinderUser));
+//[ 'id', 'name', 'isLoggedIn' ] --returns all keys as an array
+console.log(Object.values(tinderUser));
+//[ '123abc', 'Sama', false ]--returns all values of object as an array
+console.log(Object.entries(tinderUser));
+//[ [ 'id', '123abc' ], [ 'name', 'Sama' ], [ 'isLoggedIn', false ] ]--returns [key,value] pair as an array of array
 
-// console.log(tinderUser.hasOwnProperty("isLoggedIn")); //true
-// // visit console to get more properties
+console.log(tinderUser.hasOwnProperty("isLoggedIn")); //true
+// visit console to get more properties
 
 /*Object de-structure and JSON API intro*/
 
